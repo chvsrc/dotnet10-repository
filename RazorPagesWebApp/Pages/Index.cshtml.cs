7c8a7229cdf00b49a33c1176c3edc3bb0d4c743d
venkat <venkat@vm1>
2025-12-29 16:57:24 +0000

    Added razor project

---
+++ RazorPagesWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
