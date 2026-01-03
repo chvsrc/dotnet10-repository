959c90efb02df597dc1a626b18941a7a5260568f
chvsrc <89882229+chvsrc@users.noreply.github.com>
2026-01-03 11:07:17 -0500

    Integrate Swagger for API documentation
    Added Swagger UI support for API documentation in development.

--- RestApiWebApp/Program.cs
+++ RestApiWebApp/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -12,7 +14,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "My API v1");
+        options.RoutePrefix = "swagger"; // /swagger
+    });
 }
+
 
 app.UseHttpsRedirection();
 
@@ -20,6 +29,13 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapGet("/", () => "Hello World!");
+// app.MapGet("/", () => "Hello World!");
+
+// https://localhost:7155/swagger/index.html
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/swagger");
+    return Task.CompletedTask;
+});
 
 app.Run();
